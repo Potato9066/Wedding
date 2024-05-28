2ae545d6208acbfcdce1774eea671bcfd2481874
Potato9066 <tate.arredondo@icloud.com>
2024-05-28 13:30:31 -0500

    Page Setup

--- wedding.js
+++ wedding.js
@@ -5,31 +5,46 @@ function rsvp() {
 
 // NavBar
 function Home() {
-    document.getElementById("Main").innerHTML = 
-    `<p id='thing'>if i type here does it work</p>`
+    setHome()
     document.getElementById("background").className = "Home"
     window.scrollTo(0,0);
 }
 
 function Venue() {
-    document.getElementById("Main").innerHTML =
-    "<p>Felt Mansion</p>"
+    setVenue()
     document.getElementById("background").className = "Venue"
     window.scrollTo(0,0);
 }
 
 function Registry() {
-    document.getElementById("Main").innerHTML = 
-    "<p>Buy us a gift</p>"
+    setRegistry()
     document.getElementById("background").className = "Registry"
     window.scrollTo(0,0);
 }
 
 function Library() {
-    document.getElementById("Main").innerHTML =
-    "Help our Library Grow"
+    setLibrary()
     document.getElementById("background").className = "Library"
     window.scrollTo(0,0);
 }
 
 // Main Page Change
+function setHome() {
+    document.getElementById("Main").innerHTML = 
+    `<h3>Get to Know us</h3>`
+}
+
+function setVenue() {
+    document.getElementById("Main").innerHTML =
+    `<h3>Felt Mansion</h3>`
+}
+
+function setRegistry() {
+    document.getElementById("Main").innerHTML = 
+    `<h3>Buy us a Gift!`
+}
+
+function setLibrary() {
+    document.getElementById("Main").innerHTML =
+    `<h3>Help our Library Grow!</h3>`
+}
