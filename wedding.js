f7b8c3f1113ca67d967e2489e1358e52f5cc86f0
Potato9066 <tate.arredondo@icloud.com>
2024-06-20 09:45:09 -0500

    Updated layout, waiting on final words to use

--- wedding.js
+++ wedding.js
@@ -5,55 +5,21 @@ function rsvp() {
 
 // NavBar
 function Home() {
-    setHome()
-    document.getElementById("background").className = "Home"
-    window.scrollTo(0,0);
+    window.location.href = "home.html"
 }
 
 function Venue() {
-    setVenue()
-    document.getElementById("background").className = "Venue"
-    window.scrollTo(0,0);
+    window.location.href = "venue.html"
 }
 
 function Registry() {
-    setRegistry()
-    document.getElementById("background").className = "Registry"
-    window.scrollTo(0,0);
+    window.location.href = "registry.html"
 }
 
 function Library() {
-    setLibrary()
-    document.getElementById("background").className = "Library"
-    window.scrollTo(0,0);
+    window.location.href = "library.html"
 }
 
-// Main Page Change
-function setHome() {
-    document.getElementById("Main").innerHTML = 
-    `<div id="Main">
-        <div id="griddiv">
-            <img id="image" src="Images/HomePicture.jpg" id="column1">
-            <div id="column2">
-                <h2 id="center">Mateo and Megan's Wedding</h2>
-                <h4 id="center">June 29, 2025</h4>
-                <p id="center">6597 138th Ave, Holland, MI</p>
-            </div>
-        </div>
-    </div>`
-}
-
-function setVenue() {
-    document.getElementById("Main").innerHTML =
-    `<h3>Felt Mansion</h3>`
-}
-
-function setRegistry() {
-    document.getElementById("Main").innerHTML = 
-    `<h3>Buy us a Gift!`
-}
-
-function setLibrary() {
-    document.getElementById("Main").innerHTML =
-    `<h3>Help our Library Grow!</h3>`
+function Info() {
+    window.location.href = "info.html"
 }
